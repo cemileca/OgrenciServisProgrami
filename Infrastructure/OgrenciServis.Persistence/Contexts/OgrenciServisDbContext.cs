8066ab90dff4c0346e3cd57d6d810a0be0a2cbb0
Güzel İşler <97739544+cemileca@users.noreply.github.com>
2023-08-10 23:34:53 +0300

    10.08.2023 23:34

---
+++ Infrastructure/OgrenciServis.Persistence/Contexts/OgrenciServisDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciServis.Persistence.Contexts
+{
+    public class OgrenciServisDbContext : DbContext
+    {
+        public OgrenciServisDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
