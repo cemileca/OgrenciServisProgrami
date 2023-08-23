97509ce3406ec0e5a3a6de371e1d20a7a26f1c19
Güzel İşler <97739544+cemileca@users.noreply.github.com>
2023-08-23 04:14:27 +0300

    HC 23.08.2023 04:14 Tasarım

--- Presentation/OgrenciServis.WinForm/Forms/AnaSayfaFrm.cs
+++ Presentation/OgrenciServis.WinForm/Forms/AnaSayfaFrm.cs
@@ -16,5 +16,10 @@ namespace OgrenciServis.WinForm.Forms
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
